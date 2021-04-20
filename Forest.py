0d7a691106e77862662d71ece8161644dd990c1e
MarvinS23 <SimonMarvin@web.de>
2021-04-20 02:20:44 +0200

    implemented hexagonal datastructure. Need to convert data structure to plottable

--- Forest.py
+++ Forest.py
@@ -12,8 +12,9 @@ from hexalattice.hexalattice import *
 import matplotlib
 matplotlib.use("TkAgg")
 
+
 # Forest gets initialized with a specified fire distribution
-class Forest():
+class Forest:
     
     def init_random(self, rows, columns):
         for i in range(0, rows):
@@ -32,29 +33,104 @@ class Forest():
                     """
                 self.forest[i,j] = rand_state
         
-    def init_centrum(self, rows, columns):
-        for i in range(0,rows):
-            for j in range(0,columns):
+    def init_centre(self, rows, columns):
+        for i in range(0, rows):
+            for j in range(0, columns):
                 if (i == rows / 2 and j == columns / 2) or (i == (rows - 1) / 2 and j == (columns - 1) / 2):
                     self.forest[i, j] = 2
-                else: 
+                else:
                     self.forest[i, j] = 1
-                
-    def __init__(self, rows, columns, initmode='centrum'):
+
+    def init_hexa(self, rows, columns):
+        for i in range(0, rows):
+            for j in range(0, columns):
+                if (i % 2) and not (j % 2):
+                    if (i == (rows / 2) - 1 and j == columns / 2 or (i == (rows / 2) - 1 and j == (columns - 1) / 2)):# or (i == (rows / 2) - 1 and j == columns / 2 or (i == (rows / 2) - 1 and j == (columns - 1) / 2)):
+                        self.forest[i, j] = 2
+                    else:
+                        self.forest[i, j] = 1
+                if (j % 2) and not (i % 2):
+                        self.forest[i, j] = 1
+
+    def __init__(self, rows, columns, initmode='hexa'):
         #self.forest = np.empty((rows, columns)), dtype=object)
-        self.forest = np.zeros((rows,columns))
-        self.forest_new = np.zeros((rows,columns))
-        self.actions = np.zeros((rows,columns))
+        self.forest = np.zeros((rows, columns))
+        self.forest_new = np.zeros((rows, columns))
+        self.actions = np.zeros((rows, columns))
         #self.num_healthy = 0
         #self.num_fire = 0
         #self.num_burnt = 0
         if initmode == 'random':
             self.init_random(rows, columns)
-        elif initmode == 'centrum':
-            self.init_centrum(rows, columns)
+        elif initmode == 'centre':
+            self.init_centre(rows, columns)
+        elif initmode == 'hexa':
+            self.init_hexa(rows, columns)
         else: # default
             self.init_random(rows, columns)
-           
+
+    def plot_rectangular(self):
+        cmap = colors.ListedColormap(['white', 'green', 'red', 'black'])
+        bounds = [-1, 0.9, 1.9, 2.9, 4]
+        norm = colors.BoundaryNorm(bounds, cmap.N)
+        self.a.imshow(self.forest[:, :], interpolation='nearest', cmap=cmap, norm=norm)  # origin = 'higher'
+        self.fig.canvas.draw()
+
+    def plot_hex(self):
+        # hex_centers, _ = create_hex_grid(nx=self.rows,
+        #                                 ny=self.columns,
+        #                                 do_plot=False)
+        hex_centers, _ = create_hex_grid(nx=2*np.count_nonzero(self.forest[1,:])+1,
+                                         ny=2*np.count_nonzero(self.forest[:,1])+1,
+                                         do_plot=False)
+        x_hex_coords = hex_centers[:, 0]
+        y_hex_coords = hex_centers[:, 1]
+        color = np.zeros([x_hex_coords.shape[0], 3])
+        index = 0
+        for i in range(int((self.rows % 2 + self.rows) / 2)):
+            for j in range(self.columns):
+                print(self.forest[i, j])
+                print(index)
+                if 1.1 > self.forest[2 * i, j] > 0.9:
+                    # color[i * self.columns + j, :] = [0.1, 0.7, 0.2]  # green
+                    color[index, :] = [0.1, 0.7, 0.2]  # green
+                    index += 1
+                    continue
+                if 2.1 > self.forest[2 * i, j] > 1.9:
+                    color[index, :] = [0.7, 0.1, 0.1]  # red
+                    index += 1
+                    continue
+                if 3.1 > self.forest[2 * i, j] > 2.9:
+                    color[index, :] = [0, 0, 0]  # black
+                    index += 1
+                    continue
+                if 1.1 > self.forest[2 * i + 1, j] > 0.9:
+                    # color[i * self.columns + j, :] = [0.1, 0.7, 0.2]  # green
+                    color[index, :] = [0.1, 0.7, 0.2]  # green
+                    index += 1
+                    continue
+                if 2.1 > self.forest[2 * i + 1, j] > 1.9:
+                    color[index, :] = [0.7, 0.1, 0.1]  # red
+                    index += 1
+                    continue
+                if 3.1 > self.forest[2 * i + 1, j] > 2.9:
+                    color[index, :] = [0, 0, 0]  # black
+                    index += 1
+                    continue
+                # else:
+                #    color[i * self.columns + j, :] = [1, 1, 1]  # white
+        self.a.cla()
+        plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,
+                                          face_color=color,
+                                          edge_color=color,
+                                          min_diam=0.9,
+                                          plotting_gap=0.02,
+                                          rotate_deg=0,
+                                          h_ax=self.a)
+        # self.a.scatter(3, 4, color='b')
+
+        self.fig.canvas.draw()
+
     def plot(self):
         hex_centers, _ = create_hex_grid(nx=self.rows,
                                          ny=self.columns,
@@ -83,33 +159,47 @@ class Forest():
                                           plotting_gap=0.02,
                                           rotate_deg=0,
                                           h_ax=self.a)
-        #self.a.scatter(3, 4, color='b')
+        # self.a.scatter(3, 4, color='b')
 
         self.fig.canvas.draw()
 
+
 # Fire model calculates the new fire distribution on the forest for each timestep
-class Fire_model(Forest):
+class FireModel(Forest):
             
-    def __init__(self, rows, columns, initmode='centrum'):
+    def __init__(self, rows, columns, initmode='centre'):
         super().__init__(rows, columns, initmode)
         self.prob_transit = np.zeros((rows, columns))
         
     def transition(self):
         self.forest_new[:] = self.forest[:]
-        for row in range(0,self.rows):
-            for column in range(0,self.columns):
-                if self.forest[row, column] == 1: # healthy
+        for row in range(0, self.rows):
+            for column in range(0, self.columns):
+                if self.forest[row, column] == 1:  # healthy
                     fire_neighbors = self.get_number_neighbors_on_fire(row, column)
                     self.prob_transit[row, column] += 1 - self.likelihood ** fire_neighbors
-                if self.forest[row, column] == 2: # on fire
+                if self.forest[row, column] == 2:  # on fire
                     self.prob_transit[row, column] += self.beta - self.actions[row, column] * self.delta_beta
-                prob = random.randint(0,100)
+                prob = random.randint(0, 100)
                 if (self.prob_transit[row, column] * 100) > prob:
                     self.forest_new[row, column] += 1
                     self.prob_transit[row, column] = 0
         self.forest[:] = self.forest_new[:]
-        
+
+    def get_number_neighbors_with_value(self, row, column, value):
+        if column == 0 and row == 0:
+            return np.count_nonzero(self.forest[row: row + 2, column: column + 2] == value) + (self.forest[row + 2, column] == value)
+        if row >= self.rows - 2:
+            return np.count_nonzero(self.forest[row - 1: row + 2, column - 1: column + 2] == value) + (self.forest[row - 2, column] == value)
+        if column == 0:
+            return np.count_nonzero(self.forest[row - 1: row + 2, column: column + 2] == value) + (self.forest[row + 2, column] == value) + (self.forest[row - 2, column] == value)
+        if row == 0:
+            return np.count_nonzero(self.forest[row: row + 2, column - 1: column + 2] == value) + (self.forest[row + 2, column] == value)
+        return np.count_nonzero(self.forest[row - 1: row + 2, column - 1: column + 2] == value) + (self.forest[row + 2, column] == value) + (self.forest[row - 2, column] == value)
+
     def get_number_neighbors_on_fire(self, row, column):
+        return self.get_number_neighbors_with_value(row, column, 2)
+        '''
         if column == 0 and row == 0:  
             #print(self.forest[row : row + 2, column : column + 2])
             #print(np.count_nonzero(self.forest[row : row + 2, column : column + 2] == 2))
@@ -125,9 +215,10 @@ class Fire_model(Forest):
         #print(self.forest[row - 1 : row + 2, column - 1 : column + 2])
         #print(np.count_nonzero(self.forest[row - 1 : row + 2, column - 1 : column + 2] == 2))
         return np.count_nonzero(self.forest[row - 1 : row + 2, column - 1 : column + 2] == 2)
-     
-    
-class Agent_model():
+        '''
+
+
+class AgentModel():
     
     def __init__(self, rows, columns, initmode):
         super().__init__(rows, columns, initmode)
--- main.py
+++ main.py
@@ -8,74 +8,71 @@ Created on Wed Feb 24 20:28:43 2021
 
 import Forest
 import time
-from hexalattice.hexalattice import *
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import tkinter as tk
 matplotlib.interactive(True)
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-#from matplotlib.figure import Figure
-import tkinter as tk
         
     
-class simulation(Forest.Fire_model, Forest.Agent_model):
+class Simulation(Forest.FireModel, Forest.AgentModel):
     def __init__(self, GUI):
-        self.columns = 16
-        self.rows = 16
-        likelihood_to_ignite = GUI.alpha_regler.get() #3 # indicator [0, 10], higher value leads to a higher prob to ignite the neighbor trees
-        self.likelihood = 1 - likelihood_to_ignite * 0.1 # higher likelihood leads to a lower probability to ignite the trees
-        fire_agression = GUI.beta_regler.get() # indicator [0, 10]. higher value leads to a faster burning down
-        self.beta = fire_agression * 0.1#(10 - fire_agression) * 0.1
-        self.delta_beta = 0.5 # efficiency of extinguishing action
-        self.timesteps = GUI.timesteps_regler.get()
+        self.columns = 16  # can be odd or even
+        self.rows = 16  # just even
+        likelihood_to_ignite = GUI.alpha_slider.get()  #3 # indicator [0, 10], higher value leads to a higher prob to ignite the neighbor trees
+        self.likelihood = 1 - likelihood_to_ignite * 0.1  # higher likelihood leads to a lower probability to ignite the trees
+        fire_agression = GUI.beta_slider.get()  # indicator [0, 10]. higher value leads to a faster burning down
+        self.beta = fire_agression * 0.1  #(10 - fire_agression) * 0.1
+        self.delta_beta = 0.5  # efficiency of extinguishing action
+        self.timesteps = GUI.timesteps_slider.get()
         self.delta_time = 0.5
         self.fig = GUI.fig
         self.a = GUI.a
-        fire_source = 'centrum' # init random or centrum
+        fire_source = 'centre'  # init random or centre
         super().__init__(self.rows, self.columns, fire_source)
                 
 
-class GUi():
+class GUi:
 
     def __init__(self):    
         
         self.fig = plt.figure()
         self.a = self.fig.add_subplot(111)
         plt.close()
-        self.fenster = tk.Tk()
-        self.fenster.title("Forest Fire Simulation")
-        self.top_frame = tk.Frame(self.fenster)
+        self.window = tk.Tk()
+        self.window.title("Forest Fire Simulation")
+        self.top_frame = tk.Frame(self.window)
         self.top_frame.pack()
         self.left_frame = tk.Frame(self.top_frame)
         self.left_frame.pack(side=tk.LEFT)
         self.frame2 = tk.Frame(self.top_frame)
-        self.frame2.pack(side = tk.LEFT)
+        self.frame2.pack(side=tk.LEFT)
         self.frame3 = tk.Frame(self.top_frame)
-        self.frame3.pack(side = tk.RIGHT)
+        self.frame3.pack(side=tk.RIGHT)
         
-        self.bottom_frame = tk.Frame(self.fenster)
+        self.bottom_frame = tk.Frame(self.window)
         self.bottom_frame.pack(side=tk.BOTTOM)
-
         
-        self.alpha_regler = tk.Scale(self.left_frame, from_=0, to=10, orient=tk.HORIZONTAL)
-        self.alpha_regler.set(3)
-        self.alpha_regler.pack()
+        self.alpha_slider = tk.Scale(self.left_frame, from_=0, to=10, orient=tk.HORIZONTAL)
+        self.alpha_slider.set(3)
+        self.alpha_slider.pack()
         self.alpha_label = tk.Label(self.left_frame, text="Ignition Likelihood")
         self.alpha_label.pack()
         
-        self.beta_regler = tk.Scale(self.frame2, from_=0, to=10, orient=tk.HORIZONTAL)
-        self.beta_regler.set(4)
-        self.beta_regler.pack()
+        self.beta_slider = tk.Scale(self.frame2, from_=0, to=10, orient=tk.HORIZONTAL)
+        self.beta_slider.set(4)
+        self.beta_slider.pack()
         self.beta_label = tk.Label(self.frame2, text="Fire Agression")
         self.beta_label.pack()
         
-        self.timesteps_regler = tk.Scale(self.frame3, from_=0, to=20, orient=tk.HORIZONTAL)
-        self.timesteps_regler.set(3)
-        self.timesteps_regler.pack()
+        self.timesteps_slider = tk.Scale(self.frame3, from_=0, to=20, orient=tk.HORIZONTAL)
+        self.timesteps_slider.set(3)
+        self.timesteps_slider.pack()
         self.timesteps_label = tk.Label(self.frame3, text="Timesteps")
         self.timesteps_label.pack()
         
-        scenario = simulation(self)
+        scenario = Simulation(self)
         self.canvas = FigureCanvasTkAgg(scenario.fig, master=self.bottom_frame)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -87,10 +84,10 @@ class GUi():
         self.start_button = tk.Button(self.bottom_frame, text="Start", command=self.start_simulation)
         self.start_button.pack()
         
-        self.fenster.mainloop()
+        self.window.mainloop()
 
     def start_simulation(self):
-        scenario = simulation(self)
+        scenario = Simulation(self)
         scenario.plot()
         for i in range(0, scenario.timesteps):
             time.sleep(scenario.delta_time)
@@ -100,21 +97,3 @@ class GUi():
 
 
 GUI = GUi()
-
-#hex_centers, _ = create_hex_grid(nx=50,
-                                 #ny=50,
-                                 #do_plot=False)
-#x_hex_coords = hex_centers[:, 0]
-#y_hex_coords = hex_centers[:, 1]
-
-#image_path = 'deadpool.jpg'     # Works with .png, .jpg, .tif
-#colors = sample_colors_from_image_by_grid(image_path, x_hex_coords, y_hex_coords)
-#plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,
-                                      #face_color=colors,
-                                      #edge_color=colors,
-                                      #min_diam=0.9,
-                                      #plotting_gap=0.05,
-                                      #rotate_deg=0)
-
-#plt.show()
-#plt.savefig('deadpool_hexa.png')
